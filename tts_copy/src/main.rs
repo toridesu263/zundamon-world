ed9941971dc2a1668cb1c97853e79473f24486ea
xxxx yyy <xxxx@yyy.zzz>
2026-04-05 22:21:18 +0900

    winitの修正

--- tts_copy/src/main.rs
+++ tts_copy/src/main.rs
@@ -114,8 +114,8 @@ impl ApplicationHandler for App {
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
         match event {
-            WindowEvent::ScaleFactorChanged { scale_factor, inner_size_writer: _ } => {
-                log(&format!("DPIスケール変更: {:.2}", scale_factor));
+            WindowEvent::ScaleFactorChanged { inner_size_writer: mut writer, ..  } => {
+                let _ = writer.request_inner_size(winit::dpi::PhysicalSize::new(400, 100));
                 //何もしない                
             }
             WindowEvent::CloseRequested => {
